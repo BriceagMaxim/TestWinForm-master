076b1e98263c15fb4c93a71ba56bae8f6fa128f5
Briceag Maxim <briceagmaxim@gmail.com>
2021-01-19 12:08:16 +0200

    testTask

---
+++ TestTask/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿
+namespace TestTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tbMarfa = new System.Windows.Forms.TextBox();
+            this.tbCantitate = new System.Windows.Forms.TextBox();
+            this.tbPret = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtData = new System.Windows.Forms.DateTimePicker();
+            this.button6 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbPretMaxim = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.lbVenitMaxim = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(808, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Afisare";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(808, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sortare";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(808, 54);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Insert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(808, 180);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(194, 36);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Venite maxim";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(808, 264);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(194, 36);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Close";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(44, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(744, 184);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // tbMarfa
+            // 
+            this.tbMarfa.Location = new System.Drawing.Point(44, 316);
+            this.tbMarfa.Name = "tbMarfa";
+            this.tbMarfa.Size = new System.Drawing.Size(100, 26);
+            this.tbMarfa.TabIndex = 6;
+            // 
+            // tbCantitate
+            // 
+            this.tbCantitate.Location = new System.Drawing.Point(522, 316);
+            this.tbCantitate.Name = "tbCantitate";
+            this.tbCantitate.Size = new System.Drawing.Size(100, 26);
+            this.tbCantitate.TabIndex = 7;
+            // 
+            // tbPret
+            // 
+            this.tbPret.Location = new System.Drawing.Point(390, 316);
+            this.tbPret.Name = "tbPret";
+            this.tbPret.Size = new System.Drawing.Size(100, 26);
+            this.tbPret.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Marfa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Data";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(386, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Pret";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(518, 289);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Cantitate";
+            // 
+            // dtData
+            // 
+            this.dtData.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtData.Location = new System.Drawing.Point(196, 316);
+            this.dtData.Name = "dtData";
+            this.dtData.Size = new System.Drawing.Size(147, 26);
+            this.dtData.TabIndex = 13;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(808, 138);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(194, 36);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Pret maxim";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Marfa";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Data";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Pret";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Cantitate";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Suma";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // lbPretMaxim
+            // 
+            this.lbPretMaxim.AutoSize = true;
+            this.lbPretMaxim.Location = new System.Drawing.Point(40, 20);
+            this.lbPretMaxim.Name = "lbPretMaxim";
+            this.lbPretMaxim.Size = new System.Drawing.Size(95, 20);
+            this.lbPretMaxim.TabIndex = 15;
+            this.lbPretMaxim.Text = "Pret maxim: ";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(808, 222);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(194, 36);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Clear";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // lbVenitMaxim
+            // 
+            this.lbVenitMaxim.AutoSize = true;
+            this.lbVenitMaxim.Location = new System.Drawing.Point(40, 52);
+            this.lbVenitMaxim.Name = "lbVenitMaxim";
+            this.lbVenitMaxim.Size = new System.Drawing.Size(159, 20);
+            this.lbVenitMaxim.TabIndex = 17;
+            this.lbVenitMaxim.Text = "Data cu venit maxim: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 384);
+            this.Controls.Add(this.lbVenitMaxim);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.lbPretMaxim);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.dtData);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPret);
+            this.Controls.Add(this.tbCantitate);
+            this.Controls.Add(this.tbMarfa);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox tbMarfa;
+        private System.Windows.Forms.TextBox tbCantitate;
+        private System.Windows.Forms.TextBox tbPret;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtData;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.Label lbPretMaxim;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label lbVenitMaxim;
+    }
+}
---
+++ TestTask/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TestTask
+{
+    public partial class Form1 : Form
+    {
+        static string location = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "\\TestTask\\TestDB.mdf";
+        public string connection = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + location + ";Initial Catalog = TestDB; Integrated Security = True";
+        private ContextDataContext linq;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // cream instanta a clasei OleDbConnection
+            linq = new ContextDataContext(connection);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            var marfaList = linq.Vanzari.ToList();
+            foreach (var marfa in marfaList)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Tag = marfa.Id;
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[0].Value = marfa.Id;
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[1].Value = marfa.Denumire;
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[2].Value = marfa.Data;
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[3].Value = marfa.Pret;
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[4].Value = marfa.Cantitate;
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[5].Value = (marfa.Cantitate * marfa.Pret);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            var linq = new ContextDataContext(connection);
+            var marfaOrdonata = linq.Vanzari.OrderBy(el => el.Denumire).ThenBy(el => el.Data).ToList();
+            foreach (var marfa in marfaOrdonata)
+            {
+
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Tag = marfa.Id;
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[0].Value = marfa.Id;
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[1].Value = marfa.Denumire;
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[2].Value = marfa.Data;
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[3].Value = marfa.Pret;
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[4].Value = marfa.Cantitate;
+                dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[5].Value = (marfa.Cantitate * marfa.Pret);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var linq = new ContextDataContext(connection);
+            Vanzari newmarfa = new Vanzari
+            {
+                Denumire = tbMarfa.Text,
+                Data = dtData.Value,
+                Pret = Decimal.Parse(tbPret.Text),
+                Cantitate = Decimal.Parse(tbCantitate.Text)
+            };
+            linq.Vanzari.InsertOnSubmit(newmarfa);
+            linq.SubmitChanges();
+            MessageBox.Show("Datele au fost salvate cu success");
+            button7_Click(null, null);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //lbPretMaxim.Text = "Pret maxim: ";
+            var linq = new ContextDataContext(connection);
+            var pretMaxim = linq.Vanzari.Where(el => el.Denumire == tbMarfa.Text).OrderByDescending(el => el.Pret).ToList();
+            lbPretMaxim.Text += $"{pretMaxim[0].Pret} pentru {pretMaxim[0].Denumire}";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            tbMarfa.Text = tbPret.Text = tbCantitate.Text =  "";
+            lbPretMaxim.Text = "Pret maxim: ";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var linq = new ContextDataContext(connection);
+            var pretMaxim = linq.Vanzari.OrderBy(el => el.Data).ToArray();
+            decimal? suma = 0;
+            decimal? sumaMax = 0;
+            DateTime? datamax = new DateTime();
+         
+            for(int i = 1; i < pretMaxim.Length; i++)
+            {
+                if(pretMaxim[i].Data == pretMaxim[i - 1].Data)
+                {
+                    suma += pretMaxim[i].Cantitate * pretMaxim[i].Pret;
+                    datamax = pretMaxim[i].Data;
+                }
+                else
+                {
+                    sumaMax = suma;
+                    suma = 0;
+                    datamax = pretMaxim[i].Data;
+                }
+            }
+
+            lbVenitMaxim.Text = $"Data cu venit maxim : {datamax}";
+        }
+    }
+}
